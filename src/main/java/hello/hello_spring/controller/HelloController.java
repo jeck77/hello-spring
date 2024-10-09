5f94ca0e061f1e3bdae99c5a5a5c7a8dc0290f92
ajm <jeck77@naver.com>
2024-10-09 19:20:02 +0900

    API 추가

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -4,7 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.ResponseBody;
+// 단축키
+// command + p => 매개변수 보기
+// command + n => getter setter 생성
+// command + shift + enter => 만들던 코드 자동 생성
 @Controller
 public class HelloController {
 
@@ -15,11 +19,39 @@ public class HelloController {
         return "hello";
     }
 
-    // command + p 매개변수 보기
     // required 매개변수 필수 여부
     @GetMapping("hello-mvc")
     public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model){
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    // ResponseBody => http통신 Resoponse Body에 직접 넣어주겠다는 의미
+    // viewResolver` 대신에 `HttpMessageConverter`(JsonConverter, StringConverter)가 동작
+    // 즉 return 값 그대로 넘겨줌
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam(value = "name") String name, Model model){
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam(value = "name") String name, Model model){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
