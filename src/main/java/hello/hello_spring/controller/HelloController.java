a1e2ceca53e9fe422593b3125c1f9f18acf48a73
ajm <jeck77@naver.com>
2024-10-09 19:06:13 +0900

    템플릿 추가

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -3,14 +3,23 @@ package hello.hello_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
+    // GetMapping url 호출명
     @GetMapping("hello")
     public String hello(Model model){
         model.addAttribute("data", "hello!!");
         return "hello";
     }
 
+    // command + p 매개변수 보기
+    // required 매개변수 필수 여부
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
 }
