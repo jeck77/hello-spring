18c7a982c21a36bd2e668d0ca5644d1835e4054d
ajm <jeck77@naver.com>
2024-10-12 19:03:22 +0900

    직접 의존성 주입하는 방식으로 변경

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -7,9 +7,18 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class MemberController {
 
+    // 필드 주입 방법
+//    @Autowired
     private final MemberService memberService;
+//
+//
+//    // set 주입 방법
+//    @Autowired
+//    public void setMemberService(MemberService memberService) {
+//        this.memberService = memberService;
+//    }
 
-    // Autowired
+//  생성자 주입 방법
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
